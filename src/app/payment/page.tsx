cc1575631fe53b8a8f7e447d8cd7a904a7692bcc
shubhamsaini2102 <shubham2102saini@gmail.com>
2025-06-16 12:25:20 +0530

    payment-modal

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -61,7 +61,7 @@ function Payment() {
                                     </li>
                                 </ul>
                                 <div className="paybtn">
-                                    <a href="#">שלם עכשיו</a>
+                                    <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">שלם עכשיו</a>
                                 </div>
                             </div>
                         </div>
@@ -158,7 +158,225 @@ function Payment() {
                     </div>
                     </div>
                   </section>
-                
+{/* Paymentstep model one */}
+                  <div className="modal fade payemnt-modal" id="exampleModal"  aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        <div className="payment-steps">
+            <div className="payment-nav">
+              <span> <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/lt-ar.png"
+                                                    width={18}
+                                                    height={18}
+                                                    alt=""
+                                                  /></span>
+              <div className="step-count">
+                  <div className="left-step-count">
+                    <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/step-three.png"
+                                                    width={36}
+                                                    height={36}
+                                                    alt=""
+                                                  />
+                                                  STEP 3 of 4
+                  </div>
+                  <div className="right-step-count">
+                    בחר תמונה או סרטון
+                    <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/grad-user.png"
+                                                    width={40}
+                                                    height={40}
+                                                    alt=""
+                                                  />
+                                                 
+                  </div>
+              </div>
+                <span> <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/rt-ar.png"
+                                                    width={18}
+                                                    height={18}
+                                                    alt=""
+                                                  /></span>
+            </div>
+    <div className="pay-border">
+            <div className="payment-user-name">
+                <h5>User Name</h5>
+                <div className="payemt-name-holder">
+                      <div>
+                        <h4>Noel D.</h4>
+                        <h6>abc@gmail.com</h6>
+                      </div>
+                      <span>
+<Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/holdr.png"
+                                                    width={51}
+                                                    height={51}
+                                                    alt=""
+                                                  />
+                      </span>
+                </div>
+            </div>
+
+            <div className="payemt-cards">
+              <h5>סיכום הזמנה</h5>
+              <ul>
+                <li><span>300</span> כמות לייקים :</li>
+                 <li><span>10.8 </span>סה"כ לתשלום :</li>
+                  <li><span></span>התשלום חד פעמי</li>
+                   <li><span></span> <div className="form-check">
+  <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexCheckDefault">
+   אני מאשר את התקנון.
+  </label>
+</div></li>
+              </ul>
+            </div>
+            <div className="all-pay-link">
+              <p><a href="#">? לחץ כאן .</a> קוד קופון</p>
+              <a className="email-btn" href="#">youremail@example.com <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/email-blk.png"
+                                                    width={20}
+                                                    height={20}
+                                                    alt=""
+                                                  /></a>
+              <a className="pay-btn" href="#"><Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/email-whit.png"
+                                                    width={20}
+                                                    height={20}
+                                                    alt=""
+                                                  />Pay by credit card</a>
+              <a className="payment-btn" href="#"><Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/email-whit.png"
+                                                    width={20}
+                                                    height={20}
+                                                    alt=""
+                                                  />For payment with</a>
+            </div>
+            </div>
+        </div>
+      </div>
+  
+    </div>
+  </div>
+</div>
+{/* Paymentstep model two */}
+                                  <div className="modal fade payemnt-modal" id="exampleModal"  aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        <div className="payment-steps">
+            <div className="payment-nav">
+              <span> <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/lt-ar.png"
+                                                    width={18}
+                                                    height={18}
+                                                    alt=""
+                                                  /></span>
+              <div className="step-count">
+                  <div className="left-step-count">
+                    <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/step-three.png"
+                                                    width={36}
+                                                    height={36}
+                                                    alt=""
+                                                  />
+                                                  STEP 3 of 4
+                  </div>
+                  <div className="right-step-count">
+                    בחר תמונה או סרטון
+                    <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/grad-user.png"
+                                                    width={40}
+                                                    height={40}
+                                                    alt=""
+                                                  />
+                                                 
+                  </div>
+              </div>
+                <span> <Image
+                                                    className="banar-bk"
+                                                    src="/assets/images/rt-ar.png"
+                                                    width={18}
+                                                    height={18}
+                                                    alt=""
+                                                  /></span>
+            </div>
+    <div className="upload-docs">
+        <div className="upload-doc-head">
+          <div className="form-check"> 
+  <label className="form-check-label" htmlFor="flexCheckDefault">
+   בחר הכול
+  </label>
+   <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+</div>
+              <h6>ניתן לבחור מספר תמונות בקנייה אחת</h6>
+        </div>
+
+        <div className="media-view">
+          <ul>
+            <li>
+              <Image
+                          className="banar-bk"
+                          src="/assets/images/doc.png"
+                          width={90}
+                          height={90}
+                          alt=""
+                        />
+
+                         <Image
+                          className="chk"
+                          src="/assets/images/chk.png"
+                          width={18}
+                          height={18}
+                          alt=""
+                        />
+            </li>
+              <li>
+              <Image
+                          className="banar-bk"
+                          src="/assets/images/doc.png"
+                          width={90}
+                          height={90}
+                          alt=""
+                        />
+            </li>
+              <li>
+              <Image
+                          className="banar-bk"
+                          src="/assets/images/doc.png"
+                          width={90}
+                          height={90}
+                          alt=""
+                        />
+            </li>
+          </ul>
+          <a href="#" className="pay-btn">Continue</a>
+        </div>
+    </div>
+        </div>
+      </div>
+  
+    </div>
+  </div>
+</div>
             <Footer/>
     </div>
   )
